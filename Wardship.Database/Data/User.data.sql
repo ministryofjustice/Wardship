4a982973fc56ea366f573142e9ce4fd7c34a3529
carlosMoJ <carlos.fernandez@digital.justice.gov.uk>
2017-03-22 10:36:40 +0000

    Added references to pagedlist

--- Wardship.Database/Data/User.data.sql
+++ Wardship.Database/Data/User.data.sql
@@ -1,9 +1,9 @@
 ﻿IF NOT EXISTS (SELECT TOP 1 * FROM dbo.Users)
 BEGIN
 SET IDENTITY_INSERT [dbo].[Users] ON
-INSERT INTO [dbo].[Users] ([UserID], [Name], [DisplayName], [LastActive], [RoleStrength]) VALUES (1, N'matthewhearn', N'Matt', NULL, 100)
-INSERT INTO [dbo].[Users] ([UserID], [Name], [DisplayName], [LastActive], [RoleStrength]) VALUES (2, N'mdhearn', N'Matt', NULL, 100)
-INSERT INTO [dbo].[Users] ([UserID], [Name], [DisplayName], [LastActive], [RoleStrength]) VALUES (3, N'matthew.hearn@justice.gov.uk', N'Matt', NULL, 100)
+INSERT INTO [dbo].[Users] ([UserID], [Name], [DisplayName], [LastActive], [RoleStrength]) VALUES (1, N'carlosfernandez', N'Carlos local', NULL, 100)
+INSERT INTO [dbo].[Users] ([UserID], [Name], [DisplayName], [LastActive], [RoleStrength]) VALUES (2, N'eab10e', N'Carlos dom1', NULL, 100)
+INSERT INTO [dbo].[Users] ([UserID], [Name], [DisplayName], [LastActive], [RoleStrength]) VALUES (3, N'carlos.fernandez@justice.gov.uk', N'Carlos JUSTICE', NULL, 100)
 
 SET IDENTITY_INSERT [dbo].[Users] OFF
 END
