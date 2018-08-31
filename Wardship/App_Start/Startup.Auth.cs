11bf16f4df54687af29aa6ea340c99c440fb40af
carlosMoJ <carlos.fernandez@digital.justice.gov.uk>
2018-08-31 14:41:15 +0200

    modified configureauth

--- Wardship/App_Start/Startup.Auth.cs
+++ Wardship/App_Start/Startup.Auth.cs
@@ -14,35 +14,34 @@ namespace Wardship
 {
     public partial class Startup
     {
-        private static string clientId = ConfigurationManager.AppSettings["ida:ClientId"];
-        private static string aadInstance = ConfigurationManager.AppSettings["ida:AADInstance"];
-        private static string tenantId = ConfigurationManager.AppSettings["ida:TenantId"];
-        private static string postLogoutRedirectUri = ConfigurationManager.AppSettings["ida:PostLogoutRedirectUri"];
-        private static string authority = aadInstance + tenantId;
+        private string clientId = ConfigurationManager.AppSettings["ida:ClientId"];
+        private string aadInstance = ConfigurationManager.AppSettings["ida:AADInstance"];
+        private string tenantId = ConfigurationManager.AppSettings["ida:TenantId"];
+        private string postLogoutRedirectUri = ConfigurationManager.AppSettings["ida:PostLogoutRedirectUri"];
+        private string RedirectUri = ConfigurationManager.AppSettings["ida:RedirectUri"];
 
         public void ConfigureAuth(IAppBuilder app)
         {
+            if (string.IsNullOrEmpty(RedirectUri))
+            {
+                RedirectUri = "https://localhost:44397/";
+            }
             app.SetDefaultSignInAsAuthenticationType(CookieAuthenticationDefaults.AuthenticationType);
-
             //app.UseCookieAuthentication(new CookieAuthenticationOptions());
             app.UseCookieAuthentication(new CookieAuthenticationOptions
             {
                 AuthenticationType = "Cookies",
                 CookieManager = new SystemWebChunkingCookieManager()
             });
-
             OpenIdConnectAuthenticationOptions options = new OpenIdConnectAuthenticationOptions
             {
                 ClientId = clientId,
-                Authority = aadInstance + tenantId,
-                RedirectUri = ConfigurationManager.AppSettings["ida:RedirectUri"],
-                PostLogoutRedirectUri = ConfigurationManager.AppSettings["ida:PostLogoutRedirectUri"]
+                Authority = aadInstance + tenantId//,
+                                                  //PostLogoutRedirectUri = postLogoutRedirectUri,
+                                                  //RedirectUri = RedirectUri
             };
-
             app.UseOpenIdConnectAuthentication(options);
-
-
-
         }
+
     }
 }
