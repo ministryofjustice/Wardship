48d7eea5f59b27ac89c67655112c606ecc9466c9
Administrator <Administrator@EC2AMAZ-LPO9TK5>
2024-07-18 13:46:06 +0000

    try adding migration file

---
+++ Wardship/Migrations/202407181343197_AddCAFCASSInvolvedToWardshipRecord.cs
@@ -0,0 +1,30 @@
+﻿namespace Wardship.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class AddCAFCASSInvolvedToWardshipRecord : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.CAFCASSInvolvedIDs",
+                c => new
+                    {
+                        ID = c.Int(nullable: false, identity: true),
+                        Name = c.String(maxLength: 50),
+                    })
+                .PrimaryKey(t => t.ID);
+            
+            AddColumn("dbo.WardshipRecords", "CAFCASSInvolvedID", c => c.Int(nullable: false));
+            DropColumn("dbo.WardshipRecords", "CAFCASSInvolved");
+        }
+        
+        public override void Down()
+        {
+            AddColumn("dbo.WardshipRecords", "CAFCASSInvolved", c => c.Boolean(nullable: false));
+            DropColumn("dbo.WardshipRecords", "CAFCASSInvolvedID");
+            DropTable("dbo.CAFCASSInvolvedIDs");
+        }
+    }
+}
